c5371f9a35eafefe07d0b893b10c8c4996954f49
Tyler Davis <tallyndavis@comcast.net>
2021-11-13 13:32:42 -0500

    NLTK corpora download script

---
+++ indexer/management/commands/setup_nltk.py
@@ -0,0 +1,24 @@
+from django.core.management.base import BaseCommand, CommandError
+from nltk import download
+from os import environ, path
+from platform import system
+
+class Command(BaseCommand):
+    help = 'Downloads necessary NLTK corpus files'
+
+    def handle(self, *args, **options):
+        NLTK_PACKAGES_TO_INSTALL = ('stopwords',)
+
+        if 'VIRTUAL_ENV' in environ.keys():
+            download_dir = path.join(environ['VIRTUAL_ENV'], 'nltk_data')
+        elif system() == 'Darwin':
+            download_dir = '/usr/share/nltk_data'
+        elif system() == 'Windows':
+            download_dir = 'C:/nltk_data'
+        elif system() == 'Linux':
+            download_dir = '/usr/share/nltk_data'
+        else:
+            raise RuntimeError("Unrecognized system architecture: can't install nltk_data")
+
+        for package in NLTK_PACKAGES_TO_INSTALL:
+            download(package, download_dir=download_dir)
