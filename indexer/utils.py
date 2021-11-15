d44cd63a185b9a1086ef91d1cb6bb98c0a4213b7
Tyler Davis <tallyndavis@comcast.net>
2021-11-15 16:54:28 -0500

    Initial wrappers for stem, stopword, alpha

--- indexer/utils.py
+++ indexer/utils.py
@@ -1,5 +1,14 @@
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+
 def stem(word: str) -> str:
-    raise NotImplementedError
+    stemmer = PorterStemmer()
+    return stemmer.stem(word)
 
 def is_stopword(word: str) -> bool:
-    raise NotImplementedError
+    return word in stopwords.words('english')
+
+def is_alpha(word: str) -> bool:
+    return word.isalpha()
+
+# Stem words.words() and check against it for verification?
