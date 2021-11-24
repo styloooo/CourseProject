8132d0afe41002feff529ad19df502ce87ce1772
Tyler Davis <tallyndavis@comcast.net>
2021-11-24 13:05:51 -0500

    initial frontend skeleton + index view + url

--- indexer/urls.py
+++ indexer/urls.py
@@ -13,11 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
-from indexer import urls as indexer_urls
+from django.urls import path
+from indexer.views import IndexPage
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('indexer/', include(indexer_urls)),
+    path('', IndexPage.as_view()),
 ]
